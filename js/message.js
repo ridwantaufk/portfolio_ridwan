ac466878a24d4f2246d7b5987498ebf26c5217d6
Ridwan Taufik <122095756+ridwantaufk@users.noreply.github.com>
2023-03-15 10:46:46 +0700

    Update message.js

--- js/message.js
+++ js/message.js
@@ -4,17 +4,32 @@ function SubmitFormData() {
   let subject = $("#subject").val();
   let message = $("#message").val();
 
-  $.post(
-    "php/data.php",
-    {
-      name: name,
-      email: email,
-      subject: subject,
-      message: message,
-    },
-    function (data) {
-      $("#results").html(data);
-      $("#contactForm").reset();
-    }
-  );
+  if (name != "" && message != "") {
+    $.post(
+      "php/data.php",
+      {
+        name: name,
+        email: email,
+        subject: subject,
+        message: message,
+      },
+      function (data) {
+        $("#results").html(data);
+        $("#contactForm")[0].reset();
+      }
+    );
+    document.getElementById("warning-name").innerHTML = "";
+    document.getElementById("warning-message").innerHTML = "";
+  } else if (name == "") {
+    document.getElementById("warning-name").innerHTML = "*Nama Wajib Diisi !";
+    setInterval(() => {
+      document.getElementById("warning-name").innerHTML = "";
+    }, 10000);
+  } else if (message == "") {
+    document.getElementById("warning-message").innerHTML =
+      "*Pesan Wajib Diisi !";
+    setInterval(() => {
+      document.getElementById("warning-message").innerHTML = "";
+    }, 10000);
+  }
 }
